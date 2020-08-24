4ace3bda28f81ccd87faff450da19d184c7d7092
Hui <blue012617@gmail.com>
2020-08-24 16:19:35 +0800

    2st test

--- app/src/main/java/com/example/irisdemo/MainActivity.java
+++ app/src/main/java/com/example/irisdemo/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test","test1");
+        Log.d("test","test2");
     }
 }
