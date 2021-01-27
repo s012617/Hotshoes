0b9ae4363a69664cd7cf340591757c1f879af503
Hui <blue012617@gmail.com>
2021-01-27 15:17:49 +0800

    drawer

---
+++ NavDemo1/app/src/main/java/tw/com/gmall/navdemo1/SecordViewModel.java
@@ -0,0 +1,7 @@
+package tw.com.gmall.navdemo1;
+
+import androidx.lifecycle.ViewModel;
+
+public class SecordViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ NavDemo1/app/src/main/java/tw/com/gmall/navdemo1/secordFragment.java
@@ -0,0 +1,36 @@
+package tw.com.gmall.navdemo1;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class secordFragment extends Fragment {
+
+    private SecordViewModel mViewModel;
+
+    public static secordFragment newInstance() {
+        return new secordFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.secord_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(SecordViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
